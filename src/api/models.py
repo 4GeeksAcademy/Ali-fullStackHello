02f5b0a47d8c8b54eba1dfd09f640fa5aa602daa
Alimilan88 <162614275+Alimilan88@users.noreply.github.com>
2024-07-27 14:35:08 +0000

    instagram

--- src/api/models.py
+++ src/api/models.py
@@ -5,11 +5,11 @@ db = SQLAlchemy()
 
 
 class Users(db.Model):
-    __tablename__ = "users"
+    __tablename__ = "users_i"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.string, unique=True, nullable=False)
-    firstname = db.Column(db.string, unique=False, nullable=False)
-    lastname = db.Column(db.string, unique=False, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    firstname = db.Column(db.String, unique=False, nullable=False)
+    lastname = db.Column(db.String, unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
@@ -25,10 +25,18 @@ class Users(db.Model):
 
 class Followers(db.Model):
     __tablename__ = "followers"
+    id = db.Column(db.Integer, primary_key=True)
     user_from_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user_from_to = db.relationship('Users', foreign_keys=[user_from_id])
     user_to_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user_to_to = db.relationship('Users', foreign_keys=[user_to_id])
+
+    def __repr__(self):
+        return f'<Followers {self.user_from_id} - {self.user_to_id}>'
+
+    def serialize(self):
+        return {"user_from_id": self.user_from_id,
+                "user_to_id": self.user_to_id}
 
 
 class Posts(db.Model):
@@ -37,27 +45,51 @@ class Posts(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user_to = db.relationship('Users', foreign_keys=[user_id], backref=db.backref('post_to', lazy='select'))
 
+    def __repr__(self):
+        return f'<Posts {self.user_id}>'
+
+    def serialize(self):
+        return {"user_id": self.user_id}
+
 
 class Medias(db.Model):
     __tablename__ = "medias"
     id = db.Column(db.Integer, primary_key=True)
-    medias_type = db.Column(db.string, unique=True, nullable=False)
-    url = db.Column(db.string, unique=True, nullable=False)
+    medias_type = db.Column(db.String, unique=True, nullable=False)
+    url = db.Column(db.String, unique=True, nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
     post_to = db.relationship('Posts', foreign_keys=[post_id], backref=db.backref('media_to', lazy='select'))
+
+    def __repr__(self):
+        return f'<Medias {self.medias_type}>'
+
+    def serialize(self):
+        return {"id": self.id,
+                "medias_type": self.medias_type,
+                "url": self.url,
+                "post_id": self.post_id}
 
 
 class Comments(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key=True)
-    comment_text = db.Column(db.string, unique=False, nullable=False)
+    comment_text = db.Column(db.String, unique=False, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     author_to = db.relationship('Users', foreign_keys=[author_id])
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
     post_to = db.relationship('Posts', foreign_keys=[post_id], backref=db.backref('comment_to', lazy='select'))
 
+    def __repr__(self):
+        return f'<Comments {self.comment_text}>'
 
-""" class Users(db.Model):
+    def serialize(self):
+        return {"id": self.id,
+                "comment_text": self.comment_text,
+                "author_id": self.author_id,
+                "post_id": self.post_id}
+
+
+class Users(db.Model):
     __tablename__ = "users"
     # Atributos
     id = db.Column(db.Integer, primary_key=True)
@@ -112,5 +144,4 @@ class Books(db.Model):
     def serialize(self):
         return {'id': self.id,
                 'titlw': self.title,
-                'author_id': self.author_id} """
-
+                'author_id': self.author_id}
