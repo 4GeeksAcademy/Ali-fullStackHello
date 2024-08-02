0eb289477523bdf9cb8cce09251b5004f32e1e88
Alimilan88 <162614275+Alimilan88@users.noreply.github.com>
2024-08-02 18:40:06 +0000

    listo instagram Perdon por el desastre de ramas

--- src/api/routes.py
+++ src/api/routes.py
@@ -28,21 +28,21 @@ def handle_users():
         rows = db.session.execute(db.select(Users)).scalars()
         results = [row.serialize() for row in rows]  # List comprehension
         response_body['results'] = results
-        response_body['message'] = "recibí el GET request"
+        response_body['message'] = "GET received"
         return response_body, 200
+        #  CREATE USER METHOD
     if request.method == 'POST':
         data = request.json
         username = data.get('username', None)
         email = data.get('email', None)
-        # Validate the received data
         if not username or not email:
-            response_body['message'] = 'Faltan datos'
+            response_body['message'] = 'Missing data'
             response_body['results'] = {}
             return response_body, 400
         username_exist = db.session.execute(db.select(Users).where(Users.username == username)).scalar()
         email_exist = db.session.execute(db.select(Users).where(Users.email == email)).scalar()
         if username_exist or email_exist:
-            response_body['message'] = 'El usuario ya existe'
+            response_body['message'] = 'User already exist'
             response_body['results'] = {}
             return response_body, 404
         row = Users(username = data['username'], 
@@ -51,86 +51,93 @@ def handle_users():
                     lastname = data['lastname'])
         db.session.add(row)
         db.session.commit()
-        response_body['message'] = "recibí el POST request"
+        response_body['message'] = "POST received"
         return response_body, 200
-
-
-@api.route("/login", methods=["POST"])
-def login():
-    response_body = {}
-    data = request.json
-    username = data.get("username", None)
-    password = data.get("password", None)
-    #  TODO: Realizar la logica para verificar en nuestro DB
-    if username != "test" or password != "test":
-        response_body["message"] = "Bad username or password"
-        return response_body, 404
-    access_token = create_access_token(identity={"username": username, "user_id": 30})
-    response_body["message"] = "User Logged"
-    response_body["access_token"] = access_token
-    #  return jsonify(access_token=access_token)
-    return response_body, 200
-
-
-@api.route("/protected", methods=["GET"])
-@jwt_required()
-def protected():
-    response_body = {}
-    current_user = get_jwt_identity()  # Access the identity of the current user with get_jwt_identity
-    if current_user["user_id"] == 30:
-        response_body['message'] = f'Access granted to {current_user}'
-        response_body['user_data'] = current_user
-        return response_body, 200
-    response_body['message'] = f'Access denied'
-    response_body['user_data'] = {}
-    return response_body, 403
-        
-    #  return jsonify(logged_in_as=current_user), 200
 
 
 @api.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
 def handle_user(user_id):
     response_body = {}
+    #  BRING USER METHOD
     if request.method == 'GET':
         row = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
         if not row:
             response_body['results'] = {}
-            response_body['message'] = f'No existe el usuario {user_id}'
+            response_body['message'] = f'User doesnt exist {user_id}'
             return response_body, 404
         response_body['results'] = row.serialize()
-        response_body['message'] = f'recibí el GET request {user_id}'
+        response_body['message'] = f'GET received {user_id}'
         return response_body, 200
-    if request.method == 'PUT':
-        response_body['message'] = f'recibí el PUT request {user_id}'
-        return response_body, 200
-    if request.method == 'DELETE':
-        response_body['message'] = f'recibí el DELETE request {user_id}'
-        return response_body, 200
-
+        #  UPDATE USER METHOD
     if request.method == 'PUT':
         data = request.json
         username = data.get('username', None)
         email = data.get('email', None)
-        # Validate the received data
         if not username or not email:
-            response_body['message'] = 'Faltan datos'
+            response_body['message'] = 'Missing data'
             response_body['results'] = {}
             return response_body, 400
-        username_exist = db.session.execute(db.select(Users).where(Users.username == username)).scalar()
-        email_exist = db.session.execute(db.select(Users).where(Users.email == email)).scalar()
-        if username_exist or email_exist:
-            response_body['message'] = 'El usuario ya existe'
+        user = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+        if not user:
+            response_body['message'] = f'User doesnt exist {user_id}'
             response_body['results'] = {}
             return response_body, 404
-        row = Users(username = data['username'], 
-                    email = data['email'],
-                    firstname = data['firstname'],
-                    lastname = data['lastname'])
-        db.session.add(row)
+        username_exist = db.session.execute(db.select(Users).where(Users.username == username, Users.id != user_id)).scalar()
+        email_exist = db.session.execute(db.select(Users).where(Users.email == email, Users.id != user_id)).scalar()
+        if username_exist or email_exist:
+            response_body['message'] = 'User already exist'
+            response_body['results'] = {}
+            return response_body, 409
+        user.username = username
+        user.email = email
+        user.firstname = data.get('firstname', user.firstname)
+        user.lastname = data.get('lastname', user.lastname)
         db.session.commit()
-        response_body['message'] = "recibí el POST request"
+        response_body['message'] = "User updated successfully"
+        response_body['results'] = user.serialize()
         return response_body, 200
+        #  DELETE USER METHOD
+    if request.method == 'DELETE':
+        user = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+    if not user:
+        response_body['message'] = f'User doesnt exist {user_id}'
+        response_body['results'] = {}
+        return response_body, 404
+    db.session.delete(user)
+    db.session.commit()
+    response_body['message'] = f'User {user_id} deleted successfully'
+    return response_body, 200
 
+@api.route("/login", methods=["POST"])
+def login():
+    response_body = {}
+    data = request.json
+    # TODO: realizar la lógica para verificar en nuestra DB
+    email = data.get("email", None)
+    password = data.get("password", None)
+    user = db.session.execute(db.select(Users).where(Users.email == email, Users.password == password, Users.is_active == True)).scalar()
+    if not user:
+        response_body['message'] = 'Authorization denied. email, password incorrect or user inactive'
+        return response_body, 401
+    access_token = create_access_token(identity={'email': email, 'user_id': user.id, 'is_admin': user.is_admin})
+    response_body['results'] = user.serialize()
+    response_body['message'] = 'User logged'
+    response_body['access_token'] = access_token
+    return response_body, 201
+
+
+@api.route("/profile", methods=["GET"])
+@jwt_required()
+def profile():
+    response_body = {}
+    current_user = get_jwt_identity()  # Access the identity of the current user with get_jwt_identity
+    if current_user['is_admin']:
+        response_body['message'] = f'Acceso concedido a {current_user["email"]}'
+        response_body['user_data'] = current_user
+        return response_body, 200
+    response_body['message'] = f'Acceso dengado porque no eres Administrador'
+    response_body['user_data'] = {}
+    return response_body, 403
 @api.route('/followers', methods=['GET', 'POST'])
 def handle_followers():
     response_body = {}
